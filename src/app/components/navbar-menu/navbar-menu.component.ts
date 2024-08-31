82d8171e7d6a377c1ad109541e23bce40212f706
FJClavijo00 <fjclavijo00@gmail.com>
2024-08-31 20:53:18 +0200

    Creado apartado de plantillas

--- src/app/components/navbar-menu/navbar-menu.component.ts
+++ src/app/components/navbar-menu/navbar-menu.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBell, faHouse, faListOl, faMoneyBillTrendUp, faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faHouse, faListOl, faMoneyBillTrendUp, faPeopleGroup, faShirt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'navbar-menu',
@@ -27,7 +27,7 @@ export class NavbarMenuComponent {
   public equipoIcon = faPeopleGroup;
   public mercadoIcon = faMoneyBillTrendUp;
   public actividadIcon = faBell;
-
+  public plantillasIcon = faShirt;
   goHome() {
     this._router.navigateByUrl('/home');
   }
