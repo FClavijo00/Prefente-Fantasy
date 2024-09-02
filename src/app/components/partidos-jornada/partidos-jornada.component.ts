efb945445ab2c5dc84da3d11f709d118ac62ca5c
FJClavijo00 <fjclavijo00@gmail.com>
2024-09-02 18:24:18 +0200

    Implementamos modal para ver más jornadas y añadimos una data con las jornadas para iimportarlas desde ahí

--- src/app/components/partidos-jornada/partidos-jornada.component.ts
+++ src/app/components/partidos-jornada/partidos-jornada.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { Jornada, jornadas } from '../../data/calendario';
 
 @Component({
   selector: 'partidos-jornada',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbModalModule,
+    CommonModule
+  ],
   exportAs: 'partidos-jornada',
   templateUrl: './partidos-jornada.component.html',
   styleUrl: './partidos-jornada.component.css'
 })
 export class PartidosJornadaComponent {
+
+  constructor(
+    private _modalService: NgbModal
+  ) {}
+
+  public jornadasModal = jornadas;
+  public jornadaSeleccionada: Jornada | undefined;
+
+  verMasJornadas(content: any) {
+    this._modalService.open(
+      content, 
+      { size: 'lg', centered: true, backdrop: 'static', keyboard: false }
+    );
+  }
+
+
+  selectJornada(numJornada: number) {
+    this.jornadaSeleccionada = jornadas.find(jornada => jornada.numero === numJornada);
+  }
+
+  ngOnInit() {
+    this.selectJornada(1);
+  }
 
 }
---
+++ src/app/data/calendario.ts
@@ -0,0 +1,273 @@
+export interface Jornada {
+    id?: number;
+    numero: number;
+    partidos: Partido[];
+}
+
+export interface Partido {
+    id?: number;
+    descansa?: boolean;
+    fecha?: string;
+    hora?: string;
+    equipoLocal?: string;
+    equipoVisitante?: string;
+    golesLocal?: number;
+    golesVisitante?: number;
+    escudoLocal?: string;
+    escudoVisitante?: string;
+}
+
+const jornadas: Jornada[] = [
+    {
+        numero: 1,
+        partidos : [
+            {
+                fecha: 'Domingo 15 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Atlético Sabiote',
+                equipoVisitante: 'CD Castellar Íbero',
+                escudoLocal: '/assets/escudos/atco-sabiote.png',
+                escudoVisitante: '/assets/escudos/cd-castellar-ibero.png'
+            },
+            {
+                fecha: 'Domingo 15 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Arquillos CF',
+                equipoVisitante: 'CF Arroyo del Ojanco',
+                escudoLocal: '/assets/escudos/arquillos-cf.png',
+                escudoVisitante: '/assets/escudos/cf-arroyo.png'
+            },
+            {
+                fecha: 'Domingo 15 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Úbeda Rapids',
+                equipoVisitante: 'CD Canena Atlético',
+                escudoLocal: '/assets/escudos/cd-ubeda-rapids.png',
+                escudoVisitante: '/assets/escudos/canena-atco.png'
+            },
+            {
+                fecha: 'Domingo 15 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Torreperogil B',
+                equipoVisitante: 'Huesa UD',
+                escudoLocal: '/assets/escudos/cd-torreperogil.png',
+                escudoVisitante: '/assets/escudos/huesa-ud.png'
+            },
+            {
+                fecha: 'Domingo 15 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Tugia Juego Limpio',
+                equipoVisitante: 'Mogón CF',
+                escudoLocal: '/assets/escudos/cd-tugia.png',
+                escudoVisitante: '/assets/escudos/mogon-cf.png'
+            },
+            {
+                descansa: true,
+                equipoLocal: 'Jódar CF',
+                escudoLocal: '/assets/escudos/jodar-cf.png'
+            }
+        ]
+    },
+    {
+        numero: 2,
+        partidos : [
+            {
+                fecha: 'Domingo 22 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Huesa UD',
+                equipoVisitante: 'CD Tugia Juego Limpio',
+                escudoLocal: '/assets/escudos/huesa-ud.png',
+                escudoVisitante: '/assets/escudos/cd-tugia.png'
+            },
+            {
+                fecha: 'Domingo 22 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Mogón CF',
+                equipoVisitante: 'Atlético Sabiote',
+                escudoLocal: '/assets/escudos/mogon-cf.png',
+                escudoVisitante: '/assets/escudos/atco-sabiote.png'
+            },
+            {
+                fecha: 'Domingo 22 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Canena Atlético',
+                equipoVisitante: 'CD Torreperogil B',
+                escudoLocal: '/assets/escudos/canena-atco.png',
+                escudoVisitante: '/assets/escudos/cd-torreperogil.png'
+            },
+            {
+                fecha: 'Domingo 22 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Castellar Íbero',
+                equipoVisitante: 'Arquillos CF',
+                escudoLocal: '/assets/escudos/cd-castellar-ibero.png',
+                escudoVisitante: '/assets/escudos/arquillos-cf.png'
+            },
+            {
+                fecha: 'Domingo 22 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Jódar CF',
+                equipoVisitante: 'CD Úbeda Rapids',
+                escudoLocal: '/assets/escudos/jodar-cf.png',
+                escudoVisitante: '/assets/escudos/cd-ubeda-rapids.png'
+            },
+            {
+                descansa: true,
+                equipoLocal: 'CF Arroyo del Ojanco',
+                escudoLocal: '/assets/escudos/cf-arroyo.png'
+            }
+        ]
+    },
+    {
+        numero: 3,
+        partidos : [
+            {
+                fecha: 'Domingo 29 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Arquillos CF',
+                equipoVisitante: 'Mogón CF',
+                escudoLocal: '/assets/escudos/arquillos-cf.png',
+                escudoVisitante: '/assets/escudos/mogon-cf.png'
+            },
+            {
+                fecha: 'Domingo 29 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CF Arroyo del Ojanco',
+                equipoVisitante: 'CD Castellar Íbero',
+                escudoLocal: '/assets/escudos/cf-arroyo.png',
+                escudoVisitante: '/assets/escudos/cd-castellar-ibero.png'
+            },
+            {
+                fecha: 'Domingo 29 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Torreperogil B',
+                equipoVisitante: 'Jódar CF',
+                escudoLocal: '/assets/escudos/cd-torreperogil.png',
+                escudoVisitante: '/assets/escudos/jodar-cf.png'
+            },
+            {
+                fecha: 'Domingo 29 Sept.',
+                hora: '-:-',
+                equipoLocal: 'CD Tugia Juego Limpio',
+                equipoVisitante: 'CD Canena Atlético',
+                escudoLocal: '/assets/escudos/cd-tugia.png',
+                escudoVisitante: '/assets/escudos/canena-atco.png'
+            },
+            {
+                fecha: 'Domingo 29 Sept.',
+                hora: '-:-',
+                equipoLocal: 'Atlético Sabiote',
+                equipoVisitante: 'Huesa UD',
+                escudoLocal: '/assets/escudos/atco-sabiote.png',
+                escudoVisitante: '/assets/escudos/huesa-ud.png'
+            },
+            {
+                descansa: true,
+                equipoLocal: 'CD Úbeda Rapids',
+                escudoLocal: '/assets/escudos/cd-ubeda-rapids.png'
+            }
+        ]
+    },
+    {
+        numero: 4,
+        partidos : [
+            {
+                fecha: 'Domingo 6 Oct.',
+                hora: '-:-',
+                equipoLocal: 'Jódar CF',
+                equipoVisitante: 'CD Tugia Juego Limpio',
+                escudoLocal: '/assets/escudos/jodar-cf.png',
+                escudoVisitante: '/assets/escudos/cd-tugia.png'
+            },
+            {
+                fecha: 'Domingo 6 Oct.',
+                hora: '-:-',
+                equipoLocal: 'CD Canena Atlético',
+                equipoVisitante: 'Atlético Sabiote',
+                escudoLocal: '/assets/escudos/canena-atco.png',
+                escudoVisitante: '/assets/escudos/atco-sabiote.png'
+            },
+            {
+                fecha: 'Domingo 6 Oct.',
+                hora: '-:-',
+                equipoLocal: 'Huesa UD',
+                equipoVisitante: 'Arquillos CF',
+                escudoLocal: '/assets/escudos/huesa-ud.png',
+                escudoVisitante: '/assets/escudos/arquillos-cf.png'
+            },
+            {
+                fecha: 'Domingo 6 Oct.',
+                hora: '-:-',
+                equipoLocal: 'Mogón CF',
+                equipoVisitante: 'CF Arroyo del Ojanco',
+                escudoLocal: '/assets/escudos/mogon-cf.png',
+                escudoVisitante: '/assets/escudos/cf-arroyo.png'
+            },
+            {
+                fecha: 'Domingo 6 Oct.',
+                hora: '-:-',
+                equipoLocal: 'CD Úbeda Rapids',
+                equipoVisitante: 'CD Torreperogil B',
+                escudoLocal: '/assets/escudos/cd-ubeda-rapids.png',
+                escudoVisitante: '/assets/escudos/cd-torreperogil.png'
+            },
+            {
+                descansa: true,
+                equipoLocal: 'CD Castellar Íbero',
+                escudoLocal: '/assets/escudos/cd-castellar-ibero.png'
+            }
+        ]
+    },
+    {
+        numero: 5,
+        partidos : [
+            {
+                fecha: 'Domingo 13 Oct.',
+                hora: '-:-',
+                equipoLocal: 'CF Arroyo del Ojanco',
+                equipoVisitante: 'Huesa UD',
+                escudoLocal: '/assets/escudos/cf-arroyo.png',
+                escudoVisitante: '/assets/escudos/huesa-ud.png'
+            },
+            {
+                fecha: 'Domingo 13 Oct.',
+                hora: '-:-',
+                equipoLocal: 'CD Castellar Íbero',
+                equipoVisitante: 'Mogón CF',
+                escudoLocal: '/assets/escudos/cd-castellar-ibero.png',
+                escudoVisitante: '/assets/escudos/mogon-cf.png'
+            },
+            {
+                fecha: 'Domingo 13 Oct.',
+                hora: '-:-',
+                equipoLocal: 'CD Tugia Juego Limpio',
+                equipoVisitante: 'CD Úbeda Rapids',
+                escudoLocal: '/assets/escudos/cd-tugia.png',
+                escudoVisitante: '/assets/escudos/cd-ubeda-rapids.png'
+            },
+            {
+                fecha: 'Domingo 13 Oct.',
+                hora: '-:-',
+                equipoLocal: 'Atlético Sabiote',
+                equipoVisitante: 'Jódar CF',
+                escudoLocal: '/assets/escudos/atco-sabiote.png',
+                escudoVisitante: '/assets/escudos/jodar-cf.png'
+            },
+            {
+                fecha: 'Domingo 13 Oct.',
+                hora: '-:-',
+                equipoLocal: 'Arquillos CF',
+                equipoVisitante: 'CD Canena Atlético',
+                escudoLocal: '/assets/escudos/arquillos-cf.png',
+                escudoVisitante: '/assets/escudos/canena-atco.png'
+            },
+            {
+                descansa: true,
+                equipoLocal: 'CD Torreperogil B',
+                escudoLocal: '/assets/escudos/cd-torreperogil.png'
+            }
+        ]
+    }
+]
+
+export { jornadas };
