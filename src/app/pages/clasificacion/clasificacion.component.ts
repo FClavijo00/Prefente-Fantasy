8b4de4d6b85d48d2fe2e7d06d1766772df641f78
FJClavijo00 <fjclavijo00@gmail.com>
2024-09-03 18:59:04 +0200

    Clasificación Visual Finish

--- src/app/pages/clasificacion/clasificacion.component.ts
+++ src/app/pages/clasificacion/clasificacion.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { clasificacionUsuarios } from '../../data/usuarios';
+import { CommonModule } from '@angular/common';
+import { faArrowDown, faArrowUp, faMinus, faSackDollar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-clasificacion',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './clasificacion.component.html',
   styleUrl: './clasificacion.component.css'
 })
 export class ClasificacionComponent {
+
+  constructor() { }
+
+  public usuarios = clasificacionUsuarios;
+
+  public moneyIcon = faSackDollar;
+  public arrowUpIcon = faArrowUp;
+  public arrowDownIcon = faArrowDown;
+  public minusIcon = faMinus;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 
 }
