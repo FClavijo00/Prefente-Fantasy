70637bc78c810baa2363983356fd6016453da11d
FJClavijo00 <fjclavijo00@gmail.com>
2024-09-25 17:08:25 +0200

    Pantalla de Equipo y Perfil Jugador

---
+++ src/app/components/perfil-jugador/perfil-jugador.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronDown, faChevronLeft, faChevronUp, faEuroSign } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'perfil-jugador',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exportAs: 'perfil-jugador',
+  templateUrl: './perfil-jugador.component.html',
+  styleUrl: './perfil-jugador.component.css'
+})
+export class PerfilJugadorComponent {
+[x: string]: any;
+
+  @Input() modal: any;
+  @Input() jugador: any;
+  
+  @Output() cerrarModal = new EventEmitter<any>();
+
+  constructor(
+  ) { }
+
+  public jornadas = [1, 2]
+
+  public volverIcon = faChevronLeft;
+  public chevronUpIcon = faChevronUp;
+  public chevronDownIcon = faChevronDown;
+  public euroIcon = faEuroSign;
+
+  verDescripcionJornada(jornada: any) {
+    console.log(jornada);
+  }
+
+  getBarraHeight(puntos: number) {
+    if (puntos <= 0) {
+      return ;
+    }
+  }
+
+  ngOnInit(): void {
+    console.log(this.jugador)
+  }
+
+}
--- src/app/pages/equipo/equipo.component.ts
+++ src/app/pages/equipo/equipo.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfilJugadorComponent } from '../../components/perfil-jugador/perfil-jugador.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { equipo } from '../../data/equipo';
+import { jugadores } from '../../data/equipo';
+import { faChevronDown, faChevronUp, faCircleExclamation, faCircleMinus, faEuroSign } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-equipo',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    CommonModule,
+    NgbModalModule,
+    PerfilJugadorComponent,
+    TabsModule
+  ],
   templateUrl: './equipo.component.html',
   styleUrl: './equipo.component.css'
 })
 export class EquipoComponent {
+
+  constructor(
+    private _modalService: NgbModal
+  ) {}
+
+  public warningIcon              = faCircleExclamation;
+  public chevronUpIcon            = faChevronUp;
+  public chevronDownIcon          = faChevronDown;
+  public euroIcon                 = faEuroSign;
+  public minusIcon                = faCircleMinus;
+  public plantillaCompleta: any[] = [];
+  public titulares: any[]         = [];
+  public banquillo: any[]         = [];
+
+  public jugadorSeleccionado: any;
+  public jornadaActual: number    = 2;
+
+  verJugador(content: any, jugador: any) {
+    this.jugadorSeleccionado = jugador;
+    this._modalService.open(
+      content, 
+      { size: 'lg', centered: true, backdrop: 'static', keyboard: false }
+    );
+  }
+
+  ngOnInit(): void {
+    const orden = [0, 1, 2, 3];
+    this.plantillaCompleta = equipo.jugadores;
+    this.plantillaCompleta.sort((a, b) => orden.indexOf(a.orden) - orden.indexOf(b.orden));
+    this.titulares = equipo.jugadores.filter((jugador) => jugador.titular === true);
+    this.banquillo = equipo.jugadores.filter((jugador) => jugador.suplente === true);
+  }
 
 }
