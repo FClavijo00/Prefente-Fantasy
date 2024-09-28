6734e10ed66ca33862b38e04f2ef3063ae6a2587
FJClavijo00 <fjclavijo00@gmail.com>
2024-09-28 11:50:10 +0200

    Nuevos jugadores y estadísticas en equipo

--- src/app/data/equipo.ts
+++ src/app/data/equipo.ts
@@ -35,10 +35,18 @@ const jugadores: Jugadores[] = [
         titular: true,
         puntuacionJornadas: {
             1: {
-                puntos: 3
+                puntos: 3,
+                puntosXCronico: 2,
+                puntosXMinJugados: 1,
+                puntosXGoles: 0,
+                puntosXAsistencias: 0
             },
             2: {
-                puntos: 6
+                puntos: 6,
+                puntosXCronico: 5,
+                puntosXMinJugados: 1,
+                puntosXGoles: 0,
+                puntosXAsistencias: 0
             }
         }
     },
@@ -91,11 +99,11 @@ const jugadores: Jugadores[] = [
         urlImagen: 'assets/caras-futbolistas/Tugia/300x300-miguel-alcazar.png',
         urlEscudo: '/assets/escudos/cd-tugia.png',
         posicion: 'DEL',
-        posicionInicial: 'CENI',
+        posicionInicial: 'DC',
         precio: '4.500.000',
         subiendo: true,
         valorSubidaBajada: '500.000',
-        titular: true,
+        suplente: true,
         puntuacionJornadas: {
             1: {
                 puntos: 5
@@ -241,13 +249,106 @@ const jugadores: Jugadores[] = [
         titular: true,
         puntuacionJornadas: {
             1: {
-                puntos: 1
+                puntos: 1,
+                puntosXCronico: 0,
+                puntosXMinJugados: 1,
+                puntosXGoles: 0,
+                puntosXAsistencias: 0
             },
             2: {
-                puntos: 12
+                puntos: 12,
+                puntosXCronico: 8,
+                puntosXMinJugados: 1,
+                puntosXGoles: 3,
+                puntosXAsistencias: 0
             }
         }
-    }
+    },
+    {
+        orden: 0,
+        nombre: 'Pedro Jesús García Casado',
+        sobrenombre: 'Cubi',
+        urlImagen: 'assets/caras-futbolistas/Canena/300x300-cubi.png',
+        urlEscudo: '/assets/escudos/canena-atco.png',
+        posicion: 'POR',
+        posicionInicial: 'POR',
+        precio: '9.000.000',
+        bajando: true,
+        valorSubidaBajada: '100.000',
+        titular: false,
+        suplente: true,
+        puntuacionJornadas: {
+            1: {
+                puntos: null
+            },
+            2: {
+                puntos: 1
+            }
+        }
+    },
+    {
+        orden: 1,
+        nombre: 'Ángel Berbel Moreno',
+        sobrenombre: 'Ángel Berbel',
+        urlImagen: 'assets/caras-futbolistas/Huesa/300x300-berbel.png',
+        urlEscudo: '/assets/escudos/huesa-ud.png',
+        posicion: 'DEF',
+        posicionInicial: 'CENI',
+        precio: '2.000.000',
+        bajando: true,
+        valorSubidaBajada: '20.000',
+        titular: true,
+        puntuacionJornadas: {
+            1: {
+                puntos: 3
+            },
+            2: {
+                puntos: 3
+            }
+        }
+    },
+    {
+        orden: 2,
+        nombre: 'Mateo Ezequiel Flores Pizichini',
+        sobrenombre: 'Mateo Flores',
+        urlImagen: 'assets/caras-futbolistas/Rapids/300x300-mateo-flores.png',
+        urlEscudo: '/assets/escudos/cd-ubeda-rapids.png',
+        posicion: 'MED',
+        posicionInicial: 'MEDI',
+        precio: '3.000.000',
+        bajando: true,
+        valorSubidaBajada: '250.000',
+        suplente: true,
+        puntuacionJornadas: {
+            1: {
+                puntos: null
+            },
+            2: {
+                puntos: 5
+            }
+        }
+    },
+    {
+        orden: 3,
+        nombre: 'Antonio José Cano Montávez',
+        sobrenombre: 'Deko',
+        urlImagen: 'assets/caras-futbolistas/Jodar/300x300-deko.png',
+        urlEscudo: '/assets/escudos/jodar-cf.png',
+        posicion: 'DEL',
+        posicionInicial: 'EXTI',
+        precio: '5.000.000',
+        subiendo: true,
+        valorSubidaBajada: '500.000',
+        suplente: true,
+        puntuacionJornadas: {
+            1: {
+                puntos: 17
+            },
+            2: {
+                puntos: 20
+            }
+        }
+    },
 ]
 
 const equipo: Equipo = {
